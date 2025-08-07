e51eb2a4d13ee76b32d45de925423f15a7636f9d
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-07 22:18:40 +0530

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const scorpio = document.getElementById('scorpio');
 let x = window.innerWidth / 2;
 let y = window.innerHeight / 2;
+
+document.addEventListener('mousemove', (e) =>{
+    const dx = e.clientX - x;
+    const dy = e.clientY - y;
+});
