364146a29def6cd5b8c1202134d1fadd203bf5c9
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-07 22:19:47 +0530

    new commit

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ let y = window.innerHeight / 2;
 document.addEventListener('mousemove', (e) =>{
     const dx = e.clientX - x;
     const dy = e.clientY - y;
+
+    const angle = Math.atan2(dy, dx) * (180 / Math.PI);
 });
